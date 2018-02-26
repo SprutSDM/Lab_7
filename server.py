30c65c769c675f8991b9b41f29f4bad2e4b147d1
SprutSDM <zakoylov@gmail.com>
2018-02-26 14:31:34 +0300

    add theading

--- server.py
+++ server.py
@@ -1,38 +1,50 @@
 import socket
+import threading
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(levelname)s] (%(threadName)-10s) %(message)s'
+    )
+
+def client_handler(sock, address, port):
+    while True:
+        try:
+            data = sock.recv(1024)
+            print(data)
+            logging.debug('Recv: ' + data + ' from ' + adress + ':' + port)
+        except OSError:
+            break
+        
+        if len(data) == 0:
+            break
+        
+        sent_data = data
+        while True:
+            sent_len = sock.send(data)
+            if sent_len == len(data):
+                break
+            sent_data = sent_data[sent_len:]
+        logging.debug('Send: ' + data + ' from ' + adress + ':' + port)
+    
+    sock.close()
+    logging.debug('Bye-bye: ' + adress + ':' + port)
 
 def main(host='localhost', port=9090):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
     serversocket.bind((host, port))
     serversocket.listen(5)
-    
-    print('Starting Echo Server at ' + str(host) + ':' + str(port))
-    try:
-        while True:
-            clientsocket, (client_address, client_port) = serversocket.accept()
-            print('New client ' + str(client_address) + ':' + str(client_port))
-            
-            while True:
-                try:
-                    data = clientsocket.recv(1024)
-                except OSError:
-                    break
-            
-                if not data:
-                    break
-                sent_data = data
-                while True:
-                    sent_len = clientsocket.send(sent_data)
-                    if sent_len == len(data):
-                        break
-                    sent_data = sent_data[sent_len:]
-                print('Send:', data)
-            clientsocket.close()
-            print('Bye-Bye: ' + client_address + ':' + client_port)
-    except KeyboardInterrupt:
-        print('Shutting down')
-    finally:
-        serversocket.close()
+    while True:
+        try:
+            client_sock, (client_address, client_port) = serversocket.accept()
+            logging.debug('New client ' + adress + ':' + port)
+            client_thread = threading.Thread(target=client_handler,
+                            args=(client_sock, client_address, client_port))   
+            client_thread.daemon = True
+            client_thread.start()
+        except:
+            pass
 
 if __name__ == '__main__':
     main()
